0d034bdad24d482aa27fd6b4a1b9d1449f072d7c
m-pasik <m.pasik.dev@gmail.com>
2023-12-21 18:41:31 +0100

    Day 1 complete

--- 2023/01/solution.c
+++ 2023/01/solution.c
@@ -7,7 +7,8 @@ int64_t solution_p1(const char *data, size_t length)
     int64_t first = 0, last = 0;
     size_t start, end;
     for (start = 0; start < length; start++) {
-        while ((data[start] < '0' || data[start] > '9') && data[start] != '\n' && start < length)
+        while ((data[start] < '0' || data[start] > '9') &&
+               data[start] != '\n' && start < length)
             start++;
 
         if (data[start] < '0' || data[start] > '9')
@@ -25,5 +26,64 @@ int64_t solution_p1(const char *data, size_t length)
 
 int64_t solution_p2(const char *data, size_t length)
 {
-    return 0;
+    typedef struct {
+        char str[6];
+        size_t len;
+        int8_t val;
+    } Entry;
+
+#define INDEX(S) (((S)[0] ^ (S)[1]) % 128)
+#define ADD_ENTRY(M, S, V) (M)[INDEX(S)] = (Entry){ (S), strlen(S), (V) }
+#define GET_ENTRY(M, S) (M)[INDEX(S)]
+
+    Entry map[128] = { 0 };
+
+    ADD_ENTRY(map, "one", 1);
+    ADD_ENTRY(map, "two", 2);
+    ADD_ENTRY(map, "three", 3);
+    ADD_ENTRY(map, "four", 4);
+    ADD_ENTRY(map, "five", 5);
+    ADD_ENTRY(map, "six", 6);
+    ADD_ENTRY(map, "seven", 7);
+    ADD_ENTRY(map, "eight", 8);
+    ADD_ENTRY(map, "nine", 9);
+
+    int64_t first = 0, last = 0, tmp = 0;
+    size_t start, end;
+    for (start = 0; start < length; start++) {
+        for (; data[start] != '\n' && start < length; start++) {
+            if (data[start] >= '0' && data[start] <= '9') {
+                first += data[start] - '0';
+                goto found;
+            }
+
+            Entry *entry = &GET_ENTRY(map, &data[start]);
+            if (!entry->str[0])
+                continue;
+            if (start + entry->len >= length)
+                continue;
+            if (strncmp(entry->str, &data[start], entry->len) == 0) {
+                first += entry->val;
+                goto found;
+            }
+        }
+        continue;
+found:
+        for (; data[start] != '\n' && start < length; start++)
+            if (data[start] >= '0' && data[start] <= '9')
+                end = start;
+        tmp = data[end] - '0';
+
+        for (end++; data[end] != '\n' && end < length - 3; end++) {
+            Entry *entry = &GET_ENTRY(map, &data[end]);
+            if (!entry->str[0])
+                continue;
+            if (end + entry->len >= length)
+                continue;
+            if (strncmp(entry->str, &data[end], entry->len) == 0)
+                tmp = entry->val;
+        }
+        last += tmp;
+    }
+    return first * 10 + last;
 }
