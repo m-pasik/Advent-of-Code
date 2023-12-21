dd9b2d80a11566769f5afb2961503e6a9a72b29f
m-pasik <m.pasik.dev@gmail.com>
2023-12-22 00:33:45 +0100

    improved day 1 part 2 and added files for day 2

--- 2023/01/solution.c
+++ 2023/01/solution.c
@@ -1,14 +1,13 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <string.h>
 
 int64_t solution_p1(const char *data, size_t length)
 {
     int64_t first = 0, last = 0;
     size_t start, end;
     for (start = 0; start < length; start++) {
-        while ((data[start] < '0' || data[start] > '9') &&
-               data[start] != '\n' && start < length)
+        while ((data[start] < '0' || data[start] > '9') && data[start] != '\n' && start < length)
             start++;
 
         if (data[start] < '0' || data[start] > '9')
@@ -24,32 +23,36 @@ int64_t solution_p1(const char *data, size_t length)
     return first * 10 + last;
 }
 
+bool strnmatch(const char *restrict s1, const char *restrict s2, size_t n)
+{
+    for (size_t i = 0; s1[i] != '\0' && i < n; i++)
+        if (s1[i] != s2[i])
+            return false;
+    return true;
+}
+
+#define INDEX(S) (((S)[0] << 8) | (S)[1])
+#define ADD_ENTRY(M, S, V) \
+    (M)[INDEX(S)] = (Entry) { (S), (V) }
+
+typedef struct {
+    char str[6];
+    int8_t val;
+} Entry;
+
 int64_t solution_p2(const char *data, size_t length)
 {
-#define INDEX(S) (((S)[0] ^ (S)[1]) % 128)
-#define ADD_ENTRY(M, S, V)  \
-    (M)[INDEX(S)] = (Entry) \
-    {                       \
-        (S), strlen(S), (V) \
-    }
+    Entry lut[256 * 256] = { 0 };
 
-    typedef struct {
-        char str[6];
-        size_t len;
-        int8_t val;
-    } Entry;
-
-    Entry map[128] = { 0 };
-
-    ADD_ENTRY(map, "one", 1);
-    ADD_ENTRY(map, "two", 2);
-    ADD_ENTRY(map, "three", 3);
-    ADD_ENTRY(map, "four", 4);
-    ADD_ENTRY(map, "five", 5);
-    ADD_ENTRY(map, "six", 6);
-    ADD_ENTRY(map, "seven", 7);
-    ADD_ENTRY(map, "eight", 8);
-    ADD_ENTRY(map, "nine", 9);
+    ADD_ENTRY(lut, "one", 1);
+    ADD_ENTRY(lut, "two", 2);
+    ADD_ENTRY(lut, "three", 3);
+    ADD_ENTRY(lut, "four", 4);
+    ADD_ENTRY(lut, "five", 5);
+    ADD_ENTRY(lut, "six", 6);
+    ADD_ENTRY(lut, "seven", 7);
+    ADD_ENTRY(lut, "eight", 8);
+    ADD_ENTRY(lut, "nine", 9);
 
     int64_t first = 0, last = 0, tmp = 0;
     size_t start, end;
@@ -60,30 +63,26 @@ int64_t solution_p2(const char *data, size_t length)
                 goto found;
             }
 
-            Entry *entry = &map[INDEX(&data[start])];
+            Entry *entry = lut + INDEX(data + start);
             if (!entry->str[0])
                 continue;
-            if (start + entry->len >= length)
-                continue;
-            if (strncmp(entry->str, &data[start], entry->len) == 0) {
+            if (strnmatch(entry->str + 2, data + start + 2, length - start - 2)) {
                 first += entry->val;
                 goto found;
             }
         }
         continue;
-found:
+    found:
         for (; data[start] != '\n' && start < length; start++)
             if (data[start] >= '0' && data[start] <= '9')
                 end = start;
         tmp = data[end] - '0';
 
         for (end++; data[end] != '\n' && end < length - 3; end++) {
-            Entry *entry = &map[INDEX(&data[end])];
+            Entry *entry = lut + INDEX(data + end);
             if (!entry->str[0])
                 continue;
-            if (end + entry->len >= length)
-                continue;
-            if (strncmp(entry->str, &data[end], entry->len) == 0)
+            if (strnmatch(entry->str + 2, data + end + 2, length - end - 2))
                 tmp = entry->val;
         }
         last += tmp;
---
+++ 2023/02/solution.c
@@ -0,0 +1,14 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int64_t solution_p1(const char *data, size_t length)
+{
+    return 0;
+}
+
+int64_t solution_p2(const char *data, size_t length)
+{
+    return 0;
+}
